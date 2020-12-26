17b13f3eb6afdb008b9fefcd4c43538cf1f35baf
IliaLuksha <62998171+IliaLuksha@users.noreply.github.com>
2020-12-26 15:09:16 +0300

    Add files via upload

---
+++ labs/4/Data/Service1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsService1;
+
+namespace Data
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        public void OnDebug()
+        {
+            OnStart(null);
+        }
+
+        class Logger
+        {
+            Interface inter = new Interface();
+            FileSystemWatcher Watcher;
+            Info Infer = new Info();
+            Confik Parser = new Confik();
+            bool ifer;
+
+            public Logger()
+            {
+                if (File.Exists(@"C:\\Users\\User\\Desktop\\XML.xml"))
+                    Infer = Parser.XML();
+
+                else
+                {
+                    if (File.Exists(@"C:\\Users\\User\\Desktop\\JSON.json"))
+                        Infer = Parser.JSON();
+                    else
+                        EROR();
+                }
+
+                Watcher.Path = Infer.Source;
+                Watcher.IncludeSubdirectories = true;
+                Watcher.EnableRaisingEvents = true;
+                Watcher.Created += Created;
+                Watcher.Deleted += Deleted;
+                Watcher.Renamed += Renamed;
+                Watcher.Changed += Changed;
+            }
+
+            private void Created(object sender, System.IO.FileSystemEventArgs e)
+            {
+                inter.Show(inter, e.FullPath);
+            }
+
+            private void Deleted(object sender, System.IO.FileSystemEventArgs e)
+            {
+                inter.Show(inter, e.FullPath);
+            }
+
+            private void Renamed(object sender, System.IO.FileSystemEventArgs e)
+            {
+                inter.Show(inter, e.FullPath);
+            }
+
+            private void Changed(object sender, System.IO.FileSystemEventArgs e)
+            {
+                inter.Show(inter, e.FullPath);
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            logger = new Logger();
+            Thread loggerThread = new Thread(new ThreadStart(logger.Start));
+            loggerThread.Start();
+        }
+
+        protected override void OnStop()
+        {
+            logger.Stop();
+            Thread.Sleep(1000);
+        }
+        public static void EROR()
+        {
+            Console.WriteLine("ERROR. There are no configs ");
+        }
+    }
+}
+}
---
+++ labs/4/Repozitor/Repr.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelsData;
+
+namespace Repozitor
+{
+    public class Repozitor
+    {
+        private readonly string connectionString;
+
+        public Repozitor(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public Repozitor<DataCars> GetAll()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var c = new List<DataCars>();
+
+                var command = new SqlCommand("GetCarsInfo", connection)
+                {
+                    CommandType = System.Data.CommandType.StoredProcedure
+                };
+
+                var reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        var data_of_car = new DataCars();
+                        data_of_car.Name = reader.GetString(1);
+                        data_of_car.Year = reader.GetDateTime(2);
+                        data_of_car.Country = reader.GetString(3);
+                        data_of_car.Cost = reader.GetInt32(4);
+
+                        c.Add(data_of_car);
+                    }
+                }
+                reader.Close();
+
+                return c;
+
+
+
+            }
+        }
+    }
+}
